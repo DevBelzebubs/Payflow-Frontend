1fbcaaf4ca36292dded918a958cb5c8644246775
Juan Diego <juandiego.gr05@gmail.com>
2025-11-18 20:04:01 -0500

    GR-19 Added styles for user hover

--- app/dashboard/components/layout/Sidebar.tsx
+++ app/dashboard/components/layout/Sidebar.tsx
@@ -96,9 +96,9 @@ const Sidebar: React.FC<SidebarProps> = ({
         </button>
       </nav>
 
-      <div className="mt-auto p-4 border-t border-border">
-        <div className="flex items-center space-x-3 mb-4">
-          <div className="w-10 h-10 rounded-full bg-orange-100 dark:bg-orange-900/20 text-orange-600 dark:text-orange-400 flex items-center justify-center font-semibold">
+      <div className={`mt-auto border-t border-border`}>
+        <div className="flex items-center space-x-3 mb-4 hover:bg-orange-100 hover:dark:bg-orange-900/20 p-4 hover:cursor-pointer hover:text-orange-600 hover:dark:text-orange-400">
+          <div className="w-10 h-10 rounded-full bg-orange-100 dark:bg-orange-900/20 text-orange-600 dark:text-orange-400 flex items-center justify-center font-semibold ">
             {user?.nombre.charAt(0).toUpperCase()}
           </div>
           <div>
