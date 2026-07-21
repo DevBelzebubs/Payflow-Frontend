dd53fc20ef7e32da98bc7838e4f07b5c1358b9da
Juan Diego <juandiego.gr05@gmail.com>
2026-07-21 01:04:46 -0500

    GR-49 Fixed import #6

--- app/dashboard/payment/selection/[id]/page.tsx
+++ app/dashboard/payment/selection/[id]/page.tsx
@@ -1,5 +1,5 @@
+'use client';
 export const runtime = 'edge';
-'use client'
 import { getServicioById } from '@/lib/api/services/CatalogService';
 import { getMisCuentas } from '@/lib/api/services/PaymentService';
 import { Button } from '@/components/ui/button';
